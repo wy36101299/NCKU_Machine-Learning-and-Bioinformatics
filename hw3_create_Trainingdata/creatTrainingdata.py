9ef870f5eda86c503b5470cb7cca1cc83127f22d
dangyang <wy36101299@gmail.com>
2014-10-18 01:41:31 +0800

    add hw3 create trainingData

---
+++ hw3_create_Trainingdata/creatTrainingdata.py
@@ -0,0 +1,74 @@
+import glob   
+import pandas as pd
+
+class CTD(object):
+    """docstring for CTD"""
+    def __init__(self):
+        self.format_l = []
+        self.td_l = []
+        self.iternum = 0
+        self.formatname = ""
+
+    def feature(self,index):
+        format_l = self.format_l
+        feature = ((float(format_l[index+1][1])-float(format_l[index+3][1]))/float(format_l[index+1][1]))+((float(format_l[index+1][4])-float(format_l[index+3][4]))/float(format_l[index+1][4]))
+        if (feature == 0):
+            feature = 0.0001        
+        return feature
+
+    def format(self,path):
+        a = path.split('/')
+        self.formatname = a[2]
+        with open(path, 'r') as f:
+            a = f.read()
+        f = a.split('\n')
+        f.pop(0)        
+        self.iternum = len(f)-3
+        for a in range(len(f)):
+            a = f[a].split(',')
+            a.pop(0)
+            self.format_l.append(a)
+
+    def trainData(self):
+        for index in range(self.iternum):
+            format_l = self.format_l
+            classify = (float(format_l[index][3])-float(format_l[index+1][3]))/float(format_l[index+1][3])*100
+            feature = self.feature(index)
+            
+            if(classify>=2):
+                a = 2
+            elif(classify>=0 and classify<2):
+                a = 1
+            elif(classify>-2 and classify<=0):
+                a = -1
+            elif(classify<=-2):
+                a = -2
+            else:
+                a = 0
+            a = [a,feature]
+            a = a+format_l[index+1]+format_l[index+2]+format_l[index+3]
+            self.td_l.append(a)
+
+    def storage(self):
+        rowname=['classify','feature','1-open','1-high','1-low','1-close','1-volume','1-adj close','2-open','2-high','2-low','2-close','2-volume','2-adj close','3-open','3-high','3-low','3-close','3-volume','3-adj close']
+        df = pd.DataFrame(self.td_l,columns=rowname)
+        with open('./traindata/td_'+self.formatname+'.csv', 'w') as f:
+            df.to_csv(f)
+            print('td_'+self.formatname+'.csv is creat!')
+
+    def run(self):
+        path = './stock/*'   
+        paths=glob.glob(path)
+        for path in paths:
+            self.format(path)
+            self.trainData()
+            self.storage()
+
+def main():
+    ctd = CTD()
+    ctd.run()
+
+if __name__ == '__main__' :
+    main()
+
+
